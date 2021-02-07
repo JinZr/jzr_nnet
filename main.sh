c05551321551f1cdaf502b3e179ca4321086de5e
zengrui.jin@outlook.com <zengrui.jin@outlook.com>
2021-02-07 20:41:36 +0800

    updated a lot

--- main.sh
+++ main.sh
@@ -4,6 +4,7 @@ source  /project_bdda3/bdda/mengzhe/DataAugmentation/kaldi-trunk-new/egs/uaspeec
 
 EXP_PATH=/project_bdda4/bdda/zrjin/DA_AL/SI_SYS/${1}
 PROTOS_PATH=/project_bdda4/bdda/zrjin/PROTOS
+SCRIPTS_PATH=/project_bdda4/bdda/zrjin/jzr_nnet
 readonly EXP_PATH PROTOS_PATH
 mkdir -p $EXP_PATH
 
@@ -13,7 +14,8 @@ cd $EXP_PATH
 mkdir -p nnet_structures
 mkdir -p nnet_components
 
-cp $PROTOS_PATH/nnet/* nnet_components
+cp $PROTOS_PATH/nnet/protos/* nnet_components
+cp -r $PROTOS_PATH/nnet/l* nnet_components
 
 C=nnet_components
 S=nnet_structures
@@ -28,43 +30,42 @@ echo "<Rescale> <InputDim> 2000 <OutputDim> 2000 <InitParam> 0.0 <LearnRateCoef>
 nnet-initialize --binary=false $C/Rescale.proto $C/Rescale.nnet
 
 nnet-concat \
-    ./l2/final_notop.nnet \
-    ./l3/final_notop.nnet \
+    $C/l2/final_notop.nnet \
+    $C/l3/final_notop.nnet \
     ${C}/final_notop_l2l3.nnet
 nnet-concat \
-    ./l5/final_notop.nnet \
-    ./l6/final_notop.nnet \
+    $C/l5/final_notop.nnet \
+    $C/l6/final_notop.nnet \
     ${C}/final_notop_l5l6.nnet
 
-cat $PROTOS_PATH/nnet/parallel_net.proto \
-| sed "s/dim_in/4000/g" \
-| sed "s/dim_out/4000/g" \
-| sed "s/nnet1/${C}\/final_notop_l2l3.nnet/g" \
-| sed "s/nnet2/${C}\/Rescale.nnet/g" \
-> $C/parallel_net.proto
+cat $PROTOS_PATH/nnet/protos/parallel_net.proto \
+    | sed "s/dim_in/4000/g" \
+    | sed "s/dim_out/4000/g" \
+    | sed "s/nnet1/${C}\/final_notop_l2l3.nnet/g" \
+    | sed "s/nnet2/${C}\/Rescale.nnet/g" \
+    > $C/parallel_net.proto
 
 nnet-initialize $C/parallel_net.proto - | \
- nnet-concat $C/copy2_2000.nnet - $C/sum2_2000.nnet $C/final_notop_l2l3_skip.nnet
+    nnet-concat $C/copy2_2000.nnet - $C/sum2_2000.nnet $C/final_notop_l2l3_skip.nnet
 
-cat $PROTOS_PATH/nnet/parallel_net.proto \
-| sed "s/dim_in/4000/g" \
-| sed "s/dim_out/4000/g" \
-| sed "s/nnet1/${C}\/final_notop_l5l6.nnet/g" \
-| sed "s/nnet2/${C}\/Rescale.nnet/g" \
- > $C/parallel_net.proto
+cat $PROTOS_PATH/nnet/protos/parallel_net.proto \
+    | sed "s/dim_in/4000/g" \
+    | sed "s/dim_out/4000/g" \
+    | sed "s/nnet1/${C}\/final_notop_l5l6.nnet/g" \
+    | sed "s/nnet2/${C}\/Rescale.nnet/g" \
+    > $C/parallel_net.proto
 
 nnet-initialize $C/parallel_net.proto - | \
- nnet-concat $C/copy2_2000.nnet - $C/sum2_2000.nnet $C/final_notop_l5l6_skip.nnet
-
+    nnet-concat $C/copy2_2000.nnet - $C/sum2_2000.nnet $C/final_notop_l5l6_skip.nnet
 
 nnet-copy --remove-last-components=2 \
-    ./l7/final.nnet \
+    $C/l7/final.nnet \
     $C/DNN7_notop.nnet
 nnet-copy --remove-first-components=3 \
-    ./l7/final.nnet \
+    $C/l7/final.nnet \
     $C/DNN7_top.nnet
 
-./utils/make_nnet_proto.py --param-stddev-factor=0.0 100 41 0 2000 \
+python $SCRIPTS_PATH/utils/make_nnet_proto.py --param-stddev-factor=0.0 100 41 0 2000 \
     | sed "s/<AffineTransform>/<AffineTransformRMSProp>/g" \
     | sed "s/<BiasMean>/<BiasLearnRateCoef> 0.100000 <ClipWeightUpdate> 0.16 <ClipBiasUpdate> 0.16 <BiasMean>/g" \
     > $C/top_mono.proto
@@ -72,7 +73,7 @@ nnet-initialize --binary=false \
     $C/top_mono.proto \
     $C/top_mono.nnet
 
-cat parallel_net.proto \
+cat $PROTOS_PATH/nnet/protos/parallel_net.proto \
     | sed "s/dim_in/200/g" \
     | sed "s/dim_out/2042/g" \
     | sed "s/nnet1/${C}\/top_mono.nnet/g" \
@@ -80,15 +81,17 @@ cat parallel_net.proto \
     > $C/parallel_top.proto
 echo "<CopyN> 200 100 2" \
     > $C/Copy100_2.nnet
+
 nnet-initialize \
     $C/parallel_top.proto - \
     | nnet-concat $C/DNN7_notop.nnet $C/Copy100_2.nnet - \
-    $C/DNN7_MTL.nnet
+        $C/DNN7_MTL.nnet
 
 nnet-concat \
-    ./l1/final_notop.nnet \
+    --binary=false \
+    $C/l1/final_notop.nnet \
     $C/final_notop_l2l3_skip.nnet \
-    ./l4/final_notop.nnet \
+    $C/l4/final_notop.nnet \
     $C/final_notop_l5l6_skip.nnet \
     $C/DNN7_MTL.nnet \
     $S/nnet.init
