18d20a9f641e3e804ce6b460d35c106738b061fa
jinzr <zengrui.jin@outlook.com>
2021-02-09 20:20:23 +0800

    added scripts for learning

---
+++ scripts_for_learning/run_baseline_example.sh
@@ -0,0 +1,96 @@
+
+cd /project_bdda5/bdda/mengzhe/UASpeech.SVD.NN
+source /opt/share/etc/gcc-5.4.0.sh
+source my.bdda4.kaldi.path.sh
+source /project/bdda/mengzhe/cuda-11.0.sh
+
+# make the nnet
+
+cd /project_bdda3/bdda/mengzhe/DataAugmentation/kaldi-trunk-new/egs/uaspeech/s5
+
+mkdir -p dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4
+
+echo "<CopyN> 4000 2000 2" > dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/copy2_2000.nnet
+echo "<SumN> 2000 4000 2 1.0" > dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/sum2_2000.nnet
+echo "<Rescale> <InputDim> 2000 <OutputDim> 2000 <InitParam> 0.0 <LearnRateCoef> 1.0" > dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/Rescale.proto
+/project/bdda/skhu/Download/kaldi-trunk-new/src/nnetbin/nnet-initialize --binary=false dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/Rescale.proto dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/Rescale.nnet
+
+/project/bdda/skhu/Download/kaldi-trunk-new/src/nnetbin/nnet-concat dnn_fbk_wd_batchnorm_new_rmsprop/l2/final_notop.nnet dnn_fbk_wd_batchnorm_new_rmsprop/l3/final_notop.nnet dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/final_notop_l2l3.nnet
+/project/bdda/skhu/Download/kaldi-trunk-new/src/nnetbin/nnet-concat dnn_fbk_wd_batchnorm_new_rmsprop/l5/final_notop.nnet dnn_fbk_wd_batchnorm_new_rmsprop/l6/final_notop.nnet dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/final_notop_l5l6.nnet
+
+cat parallel_net.proto | sed "s/dim_in/4000/g" | sed "s/dim_out/4000/g" | sed "s/nnet1/dnn_fbk_wd_batchnorm_new_rmsprop\/all_dropout_res_MTL_mono0505_fix8decay7_bdda4\/final_notop_l2l3.nnet/g" | sed "s/nnet2/dnn_fbk_wd_batchnorm_new_rmsprop\/all_dropout_res_MTL_mono0505_fix8decay7_bdda4\/Rescale.nnet/g" > dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/parallel_net.proto
+/project/bdda/skhu/Download/kaldi-trunk-new/src/nnetbin/nnet-initialize dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/parallel_net.proto - | \
+ /project/bdda/skhu/Download/kaldi-trunk-new/src/nnetbin/nnet-concat dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/copy2_2000.nnet - dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/sum2_2000.nnet dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/final_notop_l2l3_skip.nnet
+
+cat parallel_net.proto | sed "s/dim_in/4000/g" | sed "s/dim_out/4000/g" | sed "s/nnet1/dnn_fbk_wd_batchnorm_new_rmsprop\/all_dropout_res_MTL_mono0505_fix8decay7_bdda4\/final_notop_l5l6.nnet/g" | sed "s/nnet2/dnn_fbk_wd_batchnorm_new_rmsprop\/all_dropout_res_MTL_mono0505_fix8decay7_bdda4\/Rescale.nnet/g" > dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/parallel_net.proto
+/project/bdda/skhu/Download/kaldi-trunk-new/src/nnetbin/nnet-initialize dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/parallel_net.proto - | \
+ /project/bdda/skhu/Download/kaldi-trunk-new/src/nnetbin/nnet-concat dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/copy2_2000.nnet - dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/sum2_2000.nnet dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/final_notop_l5l6_skip.nnet
+
+/project/bdda/skhu/Download/kaldi-trunk-new/src/nnetbin/nnet-copy --remove-last-components=2 dnn_fbk_wd_batchnorm_new_rmsprop/l7/final.nnet dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/DNN7_notop.nnet
+/project/bdda/skhu/Download/kaldi-trunk-new/src/nnetbin/nnet-copy --remove-first-components=3 dnn_fbk_wd_batchnorm_new_rmsprop/l7/final.nnet dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/DNN7_top.nnet
+
+utils/nnet/make_nnet_proto.py --param-stddev-factor=0.0 100 41 0 2000 | sed "s/<AffineTransform>/<AffineTransformRMSProp>/g" | sed "s/<BiasMean>/<BiasLearnRateCoef> 0.100000 <ClipWeightUpdate> 0.16 <ClipBiasUpdate> 0.16 <BiasMean>/g" > dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/top_mono.proto
+/project/bdda/skhu/Download/kaldi-trunk-new/src/nnetbin/nnet-initialize --binary=false dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/top_mono.proto dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/top_mono.nnet
+
+cat parallel_net.proto | sed "s/dim_in/200/g" | sed "s/dim_out/2042/g" | sed "s/nnet1/dnn_fbk_wd_batchnorm_new_rmsprop\/all_dropout_res_MTL_mono0505_fix8decay7_bdda4\/top_mono.nnet/g" | sed "s/nnet2/dnn_fbk_wd_batchnorm_new_rmsprop\/all_dropout_res_MTL_mono0505_fix8decay7_bdda4\/DNN7_top.nnet/g" > dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/parallel_top.proto
+echo "<CopyN> 200 100 2" > dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/Copy100_2.nnet
+/project/bdda/skhu/Download/kaldi-trunk-new/src/nnetbin/nnet-initialize dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/parallel_top.proto - | /project/bdda/skhu/Download/kaldi-trunk-new/src/nnetbin/nnet-concat dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/DNN7_notop.nnet dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/Copy100_2.nnet - dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/DNN7_MTL.nnet
+
+/project/bdda/skhu/Download/kaldi-trunk-new/src/nnetbin/nnet-concat \
+ dnn_fbk_wd_batchnorm_new_rmsprop/l1/final_notop.nnet \
+ dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/final_notop_l2l3_skip.nnet \
+ dnn_fbk_wd_batchnorm_new_rmsprop/l4/final_notop.nnet \
+ dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/final_notop_l5l6_skip.nnet \
+ dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/DNN7_MTL.nnet dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/nnet.init
+
+# run training
+
+train_scp=train_testcv_fbk_sort_kaldi.scp
+cv_scp=cv_testcv_fbk_sort_kaldi.scp
+train_label=traincv_state_mono_nosp.post
+cv_label=traincv_state_mono_nosp.post
+dbn_dir=dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4
+mlp_dir=dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4
+label_num=2001
+
+bash mlp_train_label.sh \
+ --mlp_init $mlp_dir/nnet.init \
+ --learn-rate 0.002 \
+ --copy-feats false \
+ --objective-function 'multitask,xent,41,0.5,xent,2001,0.5' \
+ --train-opts '--max-iters 0 --max-iters-fix 8 --max-iters-dec 7 --decay-factor 0.5 --momentum 0.5 --l2_penalty 0.0000025 --pre_eval true' \
+ --feature-transform context9_80.transf \
+ $mlp_dir $train_scp $cv_scp $train_label $cv_label $label_num >& dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4/dnn_all.log
+
+
+
+mv $mlp_dir/final.nnet $mlp_dir/final_MTL.nnet
+nnet-copy --remove-last-components=2 --binary=false $mlp_dir/final_MTL.nnet $mlp_dir/final_notop.nnet
+nnet-copy --remove-first-components=19 --binary=false $mlp_dir/final_MTL.nnet $mlp_dir/final_top_MTL.nnet
+perl scripts/MTL_keep_the_N.pl $mlp_dir/final_top_MTL.nnet 2 > $mlp_dir/final_top.nnet
+nnet-concat --binary=false $mlp_dir/final_notop.nnet $mlp_dir/final_top.nnet $mlp_dir/final.nnet
+
+# added by Tim
+
+train_scp=train_testcv_fbk_sort_kaldi.scp
+cv_scp=cv_testcv_fbk_sort_kaldi.scp
+train_label=traincv_state_mono_nosp.post
+cv_label=traincv_state_mono_nosp.post
+dbn_dir=dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4
+mlp_dir=dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4
+label_num=2001
+
+
+mv $mlp_dir/final.nnet $mlp_dir/final_MTL.nnet
+nnet-copy --remove-last-components=2 --binary=false $mlp_dir/final_MTL.nnet $mlp_dir/final_notop.nnet
+nnet-copy --remove-first-components=19 --binary=false $mlp_dir/final_MTL.nnet $mlp_dir/final_top_MTL.nnet
+perl scripts/MTL_keep_the_N.pl $mlp_dir/final_top_MTL.nnet 2 > $mlp_dir/final_top.nnet
+nnet-concat --binary=false $mlp_dir/final_notop.nnet $mlp_dir/final_top.nnet $mlp_dir/final.nnet
+
+cd /project_bdda3/bdda/mengzhe/DataAugmentation/kaldi-trunk-new/egs/uaspeech/s5_DataAugmentation_correct_cv
+
+bash decode.sh \
+ --scp-file /project_bdda3/bdda/mengzhe/DataAugmentation/kaldi-trunk-new/egs/uaspeech/s5/test_testcv_fbk_problem_kaldi.scp \
+ --decode-dir /project_bdda3/bdda/mengzhe/DataAugmentation/kaldi-trunk-new/egs/uaspeech/s5/hybrid255_dnn_fbk_wd_batchnorm_new_rmsprop_all_dropout_res_MTL_mono0505_fix8decay7_bdda4 \
+ --DNN-dir /project_bdda3/bdda/mengzhe/DataAugmentation/kaldi-trunk-new/egs/uaspeech/s5/dnn_fbk_wd_batchnorm_new_rmsprop/all_dropout_res_MTL_mono0505_fix8decay7_bdda4 \
+--prior_counts /project_bdda3/bdda/mengzhe/DataAugmentation/kaldi-trunk-new/egs/uaspeech/s5/ali_train_pdf.counts
